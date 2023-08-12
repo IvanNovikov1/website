39f86c1526ea7a68874e9c426f69a69b7867f5b0
IvanNovikov <sunsvetashine@gmail.com>
2023-08-12 13:14:12 +0400

    lesson 4-8

--- pythonDjango/advertisements/app_advertisements/models.py
+++ pythonDjango/advertisements/app_advertisements/models.py
@@ -1,8 +1,7 @@
 from django.db import models
+from django.contrib import admin
+from django.utils import timezone
 
-
-
-# Create your models here.
 
 class Advertisement(models.Model):
     title = models.CharField('Заголовок', max_length=128)
@@ -17,3 +16,15 @@ class Advertisement(models.Model):
 
     def __str__(self):
         return f'<Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price}'
+
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:%S')
+            return 'Сегодня в ' + str(created_time)
+        else:
+            return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+
+class Meta:
+    db_table = "advertisements"
