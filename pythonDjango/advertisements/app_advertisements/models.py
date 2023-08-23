b6d13ab35f1fc79b06750385cd5526c4def9bcd8
IvanNovikov <sunsvetashine@gmail.com>
2023-08-23 14:28:28 +0400

    lesson 4-10

--- pythonDjango/advertisements/app_advertisements/models.py
+++ pythonDjango/advertisements/app_advertisements/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib import admin
 from django.utils import timezone
 from django.utils.html import format_html
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Advertisement(models.Model):
@@ -11,6 +14,8 @@ class Advertisement(models.Model):
     auction = models.BooleanField('Торг', help_text='Укажите, уместен ли торг')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, verbose_name='пользователь', on_delete=models.CASCADE)
+    image = models.ImageField('Изображение', upload_to='advertisements/')
 
     class Meta:
         db_table = "advertisements"
@@ -27,6 +32,7 @@ class Advertisement(models.Model):
             )
         return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
 
+    @admin.display(description='Дата обновления')
     def updated_date(self):
         if self.updated_at.date() == timezone.now().date():
             updated_time = self.updated_at.strftime('%H:%M:%S')
@@ -34,6 +40,12 @@ class Advertisement(models.Model):
                 '<span style="color: red; font-weight: bold;">Сегодня в {}</span>', updated_time
             )
         return self.updated_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+    @admin.display(description='Изображение')
+    def img(self):
+        if self.image:
+            return format_html('<img src="{url}" style="width: 80px; height: 80px" alt="">', url=self.image.url)
+
 
 class Meta:
     db_table = "advertisements"
