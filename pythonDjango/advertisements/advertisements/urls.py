ab8ad9ce872c9bb301096704151ade8407546a7f
IvanNovikov <sunsvetashine@gmail.com>
2023-08-27 16:34:43 +0400

    lesson 4-11

--- pythonDjango/advertisements/advertisements/urls.py
+++ pythonDjango/advertisements/advertisements/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app_advertisements.urls')),
+    path('myauth/', include('app_auth.urls'))
 ]
 
 
--- pythonDjango/advertisements/app_advertisements/views.py
+++ pythonDjango/advertisements/app_advertisements/views.py
@@ -1,25 +1,27 @@
 from django.shortcuts import render, redirect
 from .models import Advertisement
 from .forms import AdvertisementForm
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
     advertisements = Advertisement.objects.all()
     context = {'advertisements': advertisements}
-    return render(request, 'index.html', context)
+    return render(request, 'app_advertisements/index.html', context)
 
 
 def top_sellers(request):
-    return render(request, 'top-sellers.html')
+    return render(request, 'app_advertisements/top-sellers.html')
 
 
+@login_required(login_url=reverse_lazy('login'))
 def advertisement_post(request):
     if request.method == 'POST':
         form = AdvertisementForm(request.POST, request.FILES)
         if form.is_valid():
-            advertisement = Advertisement(**form.cleaned_data)
             advertisement.user = request.user
+            advertisement = Advertisement(**form.cleaned_data)
             advertisement.save()
             url = reverse('main-page')
             return redirect(url)
@@ -28,6 +30,6 @@ def advertisement_post(request):
     else:
         form = AdvertisementForm()
         context = {'form': form}
-        return render(request, 'advertisement-post.html', context=context)
+        return render(request, 'app_advertisements/advertisement-post.html', context=context)
 
 # Create your views here.
